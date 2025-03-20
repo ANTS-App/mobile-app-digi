85cfded9dccef53a55c0f3cbc568eb7cbc03c69a
Mayur <166245029+Mayur-Gogoi@users.noreply.github.com>
2025-03-20 22:49:02 +0530

    Update build.gradle.kts
    Added code to help clone repository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ android {
 }
 
 dependencies {
+    // Added code to help clone and run repository
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     // Core AndroidX and Compose
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx.v262)
