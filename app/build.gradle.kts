2efb71bc8684cf1da937bf5f2f0f3b5d73362d58
Aditya Singh <123162244+adysinghh@users.noreply.github.com>
2025-04-06 16:30:18 +0530

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,9 +77,11 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     //implementation(libs.androidx.activity)
-    implementation("androidx.activity:activity:1.8.0")
+    implementation(libs.androidx.activity.v180)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.play.services.auth)
+
 
     // Testing
     testImplementation(libs.junit)
